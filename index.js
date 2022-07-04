25edd332752c606c96848f49bfd28a52e1d5f298
Petr Hurtak <petr.hurtak@gmail.com>
2022-07-04 14:09:17 +0200

    add plugin:react/jsx-runtime

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: [
     "plugin:react/recommended",
+    "plugin:react/jsx-runtime", // For https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/react-in-jsx-scope.md
     "plugin:react-hooks/recommended",
     "plugin:jsx-a11y/strict",
   ],
